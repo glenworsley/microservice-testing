5cc892725105b95b3ecf9b27e3066e3f01d3e512
Glen Worsley <glen.worsley@gmail.com>
2020-05-20 09:44:39 +1000

    add context load test

--- src/test/java/com/glenworsley/restdemo/RestdemoApplicationTests.java
+++ src/test/java/com/glenworsley/restdemo/RestdemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.glenworsley.restdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class RestdemoApplicationTests {
 
+	@Autowired
+	private CustomerController customerController;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(customerController);
 	}
 
 }
